56196a92b5d1b8296d5af832ba6092cba63c1e01
melvincheng <melvin-cheng@hotmail.com>
2016-03-07 21:37:58 -0500

    Script update for updated program

--- Phase1/testfiles/test.sh
+++ Phase1/testfiles/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#if a test passes, no output is produced
+
 
 #for loop to iterate through the directories that contain the test files
 for directory in {00..10};
@@ -9,28 +11,43 @@ do
 		#output which test is running
 		#extract the filename without the path
 		fileName=$(basename "$inputFile")
-		#outputs just the file name
-		echo "${fileName%.*}"
-		#outputs the
-		../../src/bank.exe < $inputFile > "${inputFile%.*}.outx"
 
+		#outputs just the file name for test indication
+		echo ${fileName%.*}
+
+		#runs the program
+		#first argument is the current accounts file
+		#seconds argument is the outputed transaction file
+		#inputs the inputfile
+		#outputs an output file
+		../../src/bank.exe $1 ${fileName%.*}.trfx < $inputFile > ${fileName%.*}.outx
+
+		#checks if file is created from the bank
+		#if file is not created, then create an empty file
+		#used for when test cases do not login
+		if [ ! -f ./${fileName%.*}.trfx ]; then
+			echo -n "" > ${fileName%.*}.trfx
+		fi
+		# 	echo -n "" > "${inputFile%.*}.trfx"
 		#checks for differences between the expected output file and actual output file
 		#-y outputs two columns, left side is the first file, the right side is the second file
 		#suppress common lines removes all lines that are matching between the files
-		diff -yZ --suppress-common-lines "${inputFile%.*}.out" "${inputFile%.*}.outx"
+		mv -f ./${fileName%.*}.trfx ./$directory
+		mv -f ./${fileName%.*}.outx ./$directory
 
-		#if a transaction file is not produced from the test, create an empty transaction file
-		#else rename the transaction file produced into a transaction file result
-		#This is requied for tests that do not login to a session
-		if [ ! -f ./transactions.trf ]; then
-			echo -n "" > "${inputFile%.*}.trfx"
-		else
-			mv -f ./transactions.trf ./"${inputFile%.*}.trfx"
-		fi
+		#move into directory to check for files
+		cd $directory
+
+		#Z ignores all trailing whitespaces
+		#checks for differences between the expected output file and actual output file
+		diff -yZ --suppress-common-lines ${fileName%.*}.out ${fileName%.*}.outx
 
 		#checks for differences between the expected transaction file and actual transaction file
-		#Z ignores all trailing whitespaces
-		diff -yZ --suppress-common-lines "${inputFile%.*}.trf" "${inputFile%.*}.trfx"
+		diff -yZ --suppress-common-lines ./${fileName%.*}.trf ./${fileName%.*}.trfx
+
+		#exit directory
+		cd ..
+
 		#newline inorder for all testcase to have their own block
 		echo ""
 	done;
