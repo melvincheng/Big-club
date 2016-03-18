ec150ded6e6f68836076f12e48ca9ce06ce786b7
Santiago Bonada <Santiago.Bonada@uoit.net>
2016-03-18 14:39:38 -0400

    sanitizing these files

--- backend/src/Account.java
+++ backend/src/Account.java
@@ -4,7 +4,7 @@
 public class Account{
   private int id; // account number
   private String name; // account holder's full name
-  private double balance; // account balance
+  private float balance; // account balance
   private boolean enabled; // flag of account if it is enabled
   private boolean student; // flag of account if it has a student plan
 
@@ -12,7 +12,7 @@ public class Account{
   * Constructor
   * all fields are set at construction of object
   */
-  public Account(int id, String name, double balance, boolean enabled, boolean student){
+  public Account(int id, String name, float balance, boolean enabled, boolean student){
     this.id = id;
     this.name = name;
     this.balance = balance;
@@ -20,45 +20,84 @@ public class Account{
     this.student = student;
   }
 
-  /**
-  * Returns value of id
-  * @return
-  */
-  public int getId() {
-    return id;
-  }
 
-  /**
-  * Returns value of name
-  * @return
-  */
-  public String getName() {
-    return name;
-  }
+	/**
+	* Returns value of id
+	* @return
+	*/
+	public int getId() {
+		return id;
+	}
 
-  /**
-  * Returns value of balance
-  * @return
-  */
-  public double getBalance(){
-    return balance;
-  }
+	/**
+	* Sets new value of id
+	* @param
+	*/
+	public void setId(int id) {
+		this.id = id;
+	}
 
+	/**
+	* Returns value of name
+	* @return
+	*/
+	public String getName() {
+		return name;
+	}
 
-  /**
-  * Returns value of enabled
-  * @return
-  */
-  public boolean isEnabled() {
-    return enabled;
-  }
+	/**
+	* Sets new value of name
+	* @param
+	*/
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  /**
-  * Returns value of student
-  * @return
-  */
-  public boolean isStudent() {
-    return student;
-  }
+	/**
+	* Returns value of balance
+	* @return
+	*/
+	public float getBalance() {
+		return balance;
+	}
 
+	/**
+	* Sets new value of balance
+	* @param
+	*/
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+
+	/**
+	* Returns value of enabled
+	* @return
+	*/
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	/**
+	* Sets new value of enabled
+	* @param
+	*/
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	/**
+	* Returns value of student
+	* @return
+	*/
+	public boolean isStudent() {
+		return student;
+	}
+
+	/**
+	* Sets new value of student
+	* @param
+	*/
+	public void setStudent(boolean student) {
+		this.student = student;
+	}
 }
