c8ea3ae118a5834459edd921d62d9acf5450170e
Santiago Bonada <Santiago.Bonada@uoit.net>
2016-03-18 19:29:08 -0400

    removed unnecessary functions

--- backend/src/Account.java
+++ backend/src/Account.java
@@ -32,27 +32,11 @@ public class Account{
 	}
 
 	/**
-	* Sets new value of id
-	* @param
-	*/
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	/**
 	* Returns value of name
 	* @return
 	*/
 	public String getName() {
 		return name;
-	}
-
-	/**
-	* Sets new value of name
-	* @param
-	*/
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	/**
